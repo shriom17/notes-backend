18f5d344a172b589cab1663ee796c9043701ce80
shriom17 <129147531+shriom17@users.noreply.github.com>
2026-03-16 11:08:56 +0530

    Server and app files have been created.

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok' });
+});
+
+export default app;
